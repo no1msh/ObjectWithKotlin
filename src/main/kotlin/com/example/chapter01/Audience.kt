23b0ad1e90a61ac865400ae55b9e863bebe1cc57
Bandal <no1msh1217@gmail.com>
2026-02-03 00:19:30 +0900

    refactor: Audience를 캡슐화 함으로써 자율적인 객체로 변경
    - TicketSeller와 Audience 사이의 결합도 낮춤

--- src/main/kotlin/com/example/chapter01/Audience.kt
+++ src/main/kotlin/com/example/chapter01/Audience.kt
@@ -2,4 +2,16 @@ package org.example.com.example.chapter01
 
 data class Audience(
     val bag: Bag,
-)
+) {
+    fun buy(ticket: Ticket): Long {
+        if (bag.hasInvitation) {
+            bag.ticket = ticket
+            return 0L
+        } else {
+            bag.ticket = ticket
+            bag.minusAmount(ticket.fee)
+            bag.ticket = ticket
+            return ticket.fee
+        }
+    }
+}
--- src/main/kotlin/com/example/chapter01/TicketSeller.kt
+++ src/main/kotlin/com/example/chapter01/TicketSeller.kt
@@ -4,14 +4,6 @@ data class TicketSeller(
     private val ticketOffice: TicketOffice,
 ) {
     fun sellTo(audience: Audience) {
-        if (audience.bag.hasInvitation) {
-            val ticket: Ticket = ticketOffice.getTicket()
-            audience.bag.ticket = ticket
-        } else {
-            val ticket: Ticket = ticketOffice.getTicket()
-            audience.bag.minusAmount(ticket.fee)
-            ticketOffice.plusAmount(ticket.fee)
-            audience.bag.ticket = ticket
-        }
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()))
     }
 }

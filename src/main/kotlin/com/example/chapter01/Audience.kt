209465bd7430fd02f82b2e5a3dd2cc23fd717133
Bandal <no1msh1217@gmail.com>
2026-02-03 00:26:53 +0900

    refactor: Bag에 캡슐화를 적용하여 자율적 객체로 변경

--- src/main/kotlin/com/example/chapter01/Audience.kt
+++ src/main/kotlin/com/example/chapter01/Audience.kt
@@ -4,14 +4,6 @@ data class Audience(
     val bag: Bag,
 ) {
     fun buy(ticket: Ticket): Long {
-        if (bag.hasInvitation) {
-            bag.ticket = ticket
-            return 0L
-        } else {
-            bag.ticket = ticket
-            bag.minusAmount(ticket.fee)
-            bag.ticket = ticket
-            return ticket.fee
-        }
+        return bag.hold(ticket)
     }
 }
